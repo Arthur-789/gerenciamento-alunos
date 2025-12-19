03e127efd21a54b21d27d1ccce1661dca89dfe88
Arthur-789 <126814057+Arthur-789@users.noreply.github.com>
2025-12-19 14:45:04 -0300

    Adiciona testes de integração para UsuarioController e AlunoController
    Adicionados 4 testes de integração para cada uma das classes dos controladores.

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,77 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.service.ServiceAluno;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ServiceAluno serviceAluno;
+
+    @Test
+    public void insertAlunosView() throws Exception {
+        MvcResult result = mockMvc.perform(get("/inserirAlunos"))
+                .andReturn();
+
+        ModelAndView mv = result.getModelAndView();
+        assertThat(mv.getViewName()).isEqualTo("Aluno/formAluno");
+        assertThat(mv.getModel().containsKey("aluno")).isTrue();
+    }
+
+    @Test
+    public void listarAlunosAdicionados() throws Exception {
+        MvcResult result = mockMvc.perform(get("/alunos-adicionados"))
+                .andReturn();
+
+        ModelAndView mv = result.getModelAndView();
+        assertThat(mv.getViewName()).isEqualTo("Aluno/listAlunos");
+        assertThat(mv.getModel().containsKey("alunosList")).isTrue();
+    }
+
+    @Test
+    public void listaAlunosAtivos() throws Exception {
+        MvcResult result = mockMvc.perform(get("/alunos-ativos"))
+                .andReturn();
+
+        ModelAndView mv = result.getModelAndView();
+        assertThat(mv.getViewName()).isEqualTo("Aluno/alunos-ativos");
+        assertThat(mv.getModel().containsKey("alunosAtivos")).isTrue();
+    }
+
+    @Test
+    public void removerAlunoRedireciona() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Aluno Remover");
+        aluno.setTurno(Turno.MATUTINO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("999999");
+        serviceAluno.save(aluno);
+
+        MvcResult result = mockMvc.perform(get("/remover/" + aluno.getId()))
+                .andReturn();
+
+        assertThat(result.getResponse().getRedirectedUrl()).isEqualTo("/alunos-adicionados");
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,67 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.model.Usuario;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void loginView() throws Exception {
+        MvcResult result = mockMvc.perform(get("/"))
+                .andReturn();
+
+        ModelAndView mv = result.getModelAndView();
+        assertThat(mv.getViewName()).isEqualTo("login/login");
+        assertThat(mv.getModel().containsKey("usuario")).isTrue();
+    }
+
+    @Test
+    public void cadastroView() throws Exception {
+        MvcResult result = mockMvc.perform(get("/cadastro"))
+                .andReturn();
+
+        ModelAndView mv = result.getModelAndView();
+        assertThat(mv.getViewName()).isEqualTo("login/cadastro");
+        assertThat(mv.getModel().containsKey("usuario")).isTrue();
+    }
+
+    @Test
+    public void logoutInvalidaSessao() throws Exception {
+        MvcResult result = mockMvc.perform(post("/logout"))
+                .andReturn();
+
+        ModelAndView mv = result.getModelAndView();
+        assertThat(mv.getViewName()).isEqualTo("login/login");
+    }
+
+    @Test
+    public void salvarUsuarioRedireciona() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("testeInt");
+        usuario.setEmail("testeInt@teste.com");
+        usuario.setSenha("1234");
+
+        MvcResult result = mockMvc.perform(post("/salvarUsuario")
+                        .flashAttr("usuario", usuario))
+                .andReturn();
+
+        assertThat(result.getResponse().getRedirectedUrl()).isEqualTo("/");
+    }
 }
